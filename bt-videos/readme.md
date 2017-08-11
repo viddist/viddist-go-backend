1fb5e4360c45fd474077b6f13151605f2717e4fc
Jacob Karlsson <jacob.karlsson95@gmail.com>
2017-08-11 18:47:20 +0200

    Implement basic bt downloading

---
+++ bt-videos/readme.md
@@ -0,0 +1,2 @@
+This is the dir where everything downloaded with bittorrent will be stored.
+This file is to keep the dir in git.
--- main.go
+++ main.go
@@ -2,8 +2,55 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/anacrolix/torrent"
 )
 
 func main() {
-	fmt.Println("Hello")
+	link := os.Args[1]
+	fmt.Println(link)
+	var clientConfig torrent.Config
+	clientConfig.DataDir = "bt-videos"
+	client, err := torrent.NewClient(&clientConfig)
+	checkErr(err)
+	defer client.Close()
+
+	bootStrapUrls := []string{
+		"router.utorrent.com",
+		"router.bittorrent.com",
+		"dht.transmissionbt.com",
+		"dht.aelitis.com",
+	}
+	//client.AddDHTNodes(bootStraps)
+	bootStrapIps := []string{}
+	for _, url := range bootStrapUrls {
+		ip, err := net.LookupIP(url)
+		checkErr(err)
+		bootStrapIps = append(bootStrapIps, ip[0].String())
+		fmt.Println("IP:", ip[0])
+	}
+	client.AddDHTNodes(bootStrapIps)
+	fmt.Println("Bootstrapped")
+
+	torr, err := client.AddMagnet(link)
+	checkErr(err)
+	<-torr.GotInfo()
+	fmt.Println("Torrent info:", torr.Info().Files)
+	fmt.Println("Added link")
+	torr.DownloadAll()
+
+	if client.WaitAll() {
+		log.Print("Downloaded the torrent")
+	} else {
+		log.Fatal("Did not finish downloading for some reason")
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalf("error creating client: %s", err)
+	}
 }
