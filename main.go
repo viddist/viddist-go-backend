fba7fe731f9e6c990dcbfb01ba7590e6086fa721
Jacob Karlsson <jacob.karlsson95@gmail.com>
2017-08-24 20:40:45 +0200

    Add basic cli args

--- main.go
+++ main.go
@@ -11,11 +11,32 @@ import (
 
 	"github.com/anacrolix/dht"
 	"github.com/anacrolix/torrent"
+	"github.com/urfave/cli"
 )
 
 func main() {
-	link := os.Args[1]
+	app := cli.NewApp()
 
+	app.Commands = []cli.Command{
+		{
+			Name:    "get",
+			Aliases: []string{"g"},
+			Usage:   "Download a magnet link or ipfs hash",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Downloading", c.Args().First())
+				dlMagnet(c.Args().First())
+				return nil
+			},
+		},
+	}
+
+	fmt.Println(C.random())
+
+	app.Run(os.Args)
+}
+
+func dlMagnet(magnet string) {
+	fmt.Println("in the func")
 	addrs, err := dht.GlobalBootstrapAddrs()
 	checkErr(err)
 	var clientConfig torrent.Config
@@ -24,13 +45,14 @@ func main() {
 	clientConfig.DataDir = "bt-videos"
 	clientConfig.DHTConfig = dhtConfig
 	client, err := torrent.NewClient(&clientConfig)
+
 	// I'm not entirely sure this config is working since eduroam seems
 	// to mess with the dht. Test someplace else.
 	//client, err := torrent.NewClient(nil)
 	checkErr(err)
 	defer client.Close()
 
-	torr, err := client.AddMagnet(link)
+	torr, err := client.AddMagnet(magnet)
 	checkErr(err)
 	<-torr.GotInfo()
 	fmt.Println("Torrent info:", torr.Info().Files)
@@ -42,8 +64,6 @@ func main() {
 	} else {
 		log.Fatal("Did not finish downloading for some reason")
 	}
-
-	fmt.Println(C.random())
 }
 
 func checkErr(err error) {
